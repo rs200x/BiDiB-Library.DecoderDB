3f1b7aaadb7c59ba8088b0e2aa099ac6991abd2f
Robert Kölz <rknoseman@hotmail.de>
2025-02-06 12:08:01 +0100

    add base structure

---
+++ BiDiB-Library.DecoderDB/DependencyExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+
+namespace org.bidib.Net.DecoderDB;
+
+public static class DependencyExtensions
+{
+    public static void AddDecoderDB(this IServiceCollection services)
+    {
+    }
+}
---
+++ BiDiB-Library.DecoderDB/readme.md
@@ -0,0 +1,3 @@
+# BiDiB-Library.DecoderDB
+
+BiDiB-Library extension for managing loco/device decoder and firmware data provided by DecoderDB
